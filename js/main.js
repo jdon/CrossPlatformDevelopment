74f20caa8e7406847183c2de6811362f23f52696
Jdon <jdon@users.noreply.github.com>
2018-05-02 16:34:44 +0100

    Added links

--- js/main.js
+++ js/main.js
@@ -47,6 +47,9 @@ function showItem(ID,type){
     var pictureURL = item.PictureURL;
     var Category = item.ItemDescription;
     var AveragePrice = item.AveragePrice;
+    var LowestPrice = item.LowestPrice;
+    var SearchURL = item.SearchURL;
+    console.log("search URL"+SearchURL);
     console.log("type:"+type);
     if(type == 0)
     {
@@ -65,9 +68,11 @@ function showItem(ID,type){
     {
         //Item page
         $("#ItemPic").html('<img src="'+pictureURL+'"/>');
-        $("#ItemName").html( "<p>"+title+"</p>" );
-        $("#ItemDescription").html( "<p>"+Category+"</p>" );
-        $("#ItemPrice").html( "<p>£"+AveragePrice+"</p>" );
+        $("#ItemName").html( "<h5>"+title+"</h5>" );
+        $("#ItemDescription").html( "<p>Category:      "+Category+"</p>" );
+        $("#AveragePrice").html(    "<p>Average Price:£"+AveragePrice+"</p>" );
+        $("#LowestPrice").html(     "<p>Lowest Price: £"+LowestPrice+"</p>" );
+        $("#EbayButton").html("<a href=\"#\" onclick=\"window.open('"+SearchURL+"', '_system');\" class=\"ui-btn ui-btn-b ui-shadow ui-corner-all\">View on Ebay</a>");
     }
 }
 
@@ -136,59 +141,76 @@ function LookupProduct(EAN)
             stat = data[0].ack[0];
             if(stat != null){
                 if(stat == "Success"){
-                    if(data[0].searchResult[0].item != null){
-                        items = data[0].searchResult[0].item;
-                        tesd = items[0];
-                        var lowestPrice = 999999999;
-                        var totalPrice = 0;
-                        var product = new Object();
-                        for(i = 0; i < items.length;i++)
+                    if(data[0].searchResult.length > 0){
+                        if(data[0].searchResult[0].item != null)
                         {
-                            console.log("Adding items")
-                            item = items[i];
-                            console.log(item)
-                            title = item.title[0];
-                            imageURL = item.galleryURL[0];
-                            sellingStatus = item.sellingStatus[0];
-                            ConvertedPrice = sellingStatus.convertedCurrentPrice[0];
-                            price = ConvertedPrice.__value__;
-                            category = item.primaryCategory[0];
-                            ItemDescription = category.categoryName[0];
-                            totalPrice = totalPrice + parseInt(price);
-                            console.log("Adding items 2")
-                            if(price < lowestPrice)
+                            var SearchURL = data[0].itemSearchURL[0]
+                            var items = data[0].searchResult[0].item;
+                            var tesd = items[0];
+                            var lowestPrice = 999999999;
+                            var totalPrice = 0;
+                            var product = new Object();
+                            for(i = 0; i < items.length;i++)
                             {
-                                lowestPrice = price;
+                                //console.log("Adding items")
+                                item = items[i];
+                                //console.log(item)
+                                title = item.title[0];
+                                //try to get a high quality image, if not use a normal image
+                                if(item.pictureURLSuperSize == null)
+                                {
+                                    PictureURL = item.galleryURL[0];
+                                }else
+                                    {
+                                        PictureURL = item.pictureURLSuperSize[0];
+                                    }
+                                var imageURL = item.galleryURL[0];
+                                var sellingStatus = item.sellingStatus[0];
+                                var ConvertedPrice = sellingStatus.convertedCurrentPrice[0];
+                                var price = ConvertedPrice.__value__;
+                                var category = item.primaryCategory[0];
+                                var ItemDescription = category.categoryName[0];
+                                totalPrice = totalPrice + parseInt(price);
+                                //console.log("Adding items 2")
+                                if(price*10 < lowestPrice)
+                                {
+                                    console.log("Lowest Price: "+price);
+                                    lowestPrice = price;
+                                }
+                                if(i == 0)
+                                {
+                                    product.title = title;
+                                    product.SearchURL = SearchURL;
+                                    product.PictureURL = PictureURL;
+                                    product.AveragePrice = 0;
+                                    product.LowestPrice = 0;
+                                    product.Category = category;
+                                    product.ItemDescription = ItemDescription;
+                                }
+                                //console.log("Adding items 3")
                             }
-                            if(i == 0)
-                            {
-                                product.title = title;
-                                product.PictureURL = item.pictureURLSuperSize[0];
-                                product.AveragePrice = 0;
-                                product.LowestPrice = 0;
-                                product.Category = category;
-                                product.ItemDescription = ItemDescription;
-                            }
-                            console.log("Adding items 3")
-                        }
-                        console.log("finished loop")
-                        product.LowestPrice = lowestPrice;
-                        console.log(totalPrice);
-                        averagePrice = (totalPrice/items.length).toFixed(2);;
-                        console.log(averagePrice);
-                        product.AveragePrice = averagePrice;
-                        console.log("calling add Item")
-                        addItem(product);
+                            console.log("finished loop")
+                            product.LowestPrice = lowestPrice;
+                            console.log(totalPrice);
+                            averagePrice = (totalPrice/items.length).toFixed(2);;
+                            console.log(averagePrice);
+                            product.AveragePrice = averagePrice;
+                            console.log("calling add Item")
+                            addItem(product);
 
-                        //scroll to bottom of page
-                        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+                            //scroll to bottom of page
+                            $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+                        }else
+                            {
+                                alert("Could not find product");
+                            }
                     }else
                     {
                         alert("Could not find product");
                     }
                 }else
                 {
-                    alert("Search unsucessful");
+                    alert("Could not find product");
                 }
             }else
             {
@@ -197,6 +219,6 @@ function LookupProduct(EAN)
         });
     }).error(function(jqXhr, textStatus, error)
     {
-        alert(textStatus);
+        alert("Unable to connect");
     })
 }
